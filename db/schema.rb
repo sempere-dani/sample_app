69ee3f9a258dfaa89db013b78f4af20d21ad50b0
sempere-dani <sempere.dani@gmail.com>
2015-08-10 17:27:36 +0200

    Created login form and gravatar

--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,6 @@ ActiveRecord::Schema.define(version: 20150810095256) do
     t.string   "password_digest"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email"
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
